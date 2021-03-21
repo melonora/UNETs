0115652d5470ba29f283fa1fa72749c69203b56c
wmv_hpomen <w-mv@hotmail.com>
2021-03-21 19:40:49 +0100

    Added pyment numpy docstyle documentation to be filled in

--- src/unet_parts.py
+++ src/unet_parts.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class Unet_block(nn.Module):
-    """ Standard block as used in the encoder and decoder in UNET paper"""
+    """Standard block as used in the encoder and decoder in UNET paper"""
     def __init__(self, ch_in, ch_out):
         super().__init__()
         self.conv1 = nn.conv2d(ch_in, ch_out, 3)
@@ -21,16 +21,29 @@ class Unet_block(nn.Module):
         Returns
         -------
 
+        
         """
         return self.relu(self.conv2(self.relu(self.conv1(x))))
 
 class UNET_encoder(nn.Module):
+    """ """
     def __init__(self, channels = (3, 64, 128, 256. 512. 1024)):
         super().__init__()
         self.encode_UNET = nn.MolduleList([Unet_block(channels[i], channels[i+1]) for i in range(len(channels)-1)])
         self.mpool = nn.MaxPool2d(2)
 
     def forward(self, x):
+        """
+
+        Parameters
+        ----------
+        x :
+            
+
+        Returns
+        -------
+
+        """
         features = []
         for u_block in self.encode_UNET:
             x = u_block(x)
@@ -39,6 +52,7 @@ class UNET_encoder(nn.Module):
         return features
 
 class UNET_decoder(nn.Module):
+    """ """
     def __init__(self, channels = (1024, 512, 256, 128, 64)):
         super().__init__()
         self.upconvs = nn.ModuleList([nn.ConvTranspose2d(channels[i], channels[i+1], 2, 2) for i in range(len(channels)-1)])
@@ -46,11 +60,37 @@ class UNET_decoder(nn.Module):
         self.channels = channels
 
     def crop(self, encoder_feature, x):
+        """
+
+        Parameters
+        ----------
+        encoder_feature :
+            
+        x :
+            
+
+        Returns
+        -------
+
+        """
         _, _, H, W = x.shape
         encoder_feature = torchvision.transforms.CenterCrop([H, W])(encoder_features)
         return encoder_features
 
     def forward(self, x, encoder_features):
+        """
+
+        Parameters
+        ----------
+        x :
+            
+        encoder_features :
+            
+
+        Returns
+        -------
+
+        """
         for i in range(len(self.channels)-1):
             x = self.upconvs[i](x)
             encoder_feature = self.crop(encoder_features[i], x)
