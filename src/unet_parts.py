788c180ff1174fe20e76bff587502e0a77b774c6
wmv_hpomen <w-mv@hotmail.com>
2021-03-21 18:38:28 +0100

    Added encoder class for UNET

--- src/unet_parts.py
+++ src/unet_parts.py
@@ -24,4 +24,16 @@ class Unet_block(nn.Module):
         """
         return self.relu(self.conv2(self.relu(self.conv1(x))))
 
+class UNET_encoder(nn.Module):
+    def __init__(self, channels = (3, 64, 128, 256. 512. 1024)):
+        super().__init__()
+        self.encode_UNET = nn.MolduleList([Unet_block(channels[i], channels[i+1]) for i in range(len(channels)-1)])
+        self.mpool = nn.MaxPool2d(2)
 
+    def forward(self, x):
+        features = []
+        for u_block in self.encode_UNET:
+            x = u_block(x)
+            features.append(x)
+            x = self.pool(x)
+        return features
