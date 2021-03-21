e2eb3625bd75ec97646cf3e256e5125c835493fc
wmv_hpomen <w-mv@hotmail.com>
2021-03-21 22:18:31 +0100

    Added documentation and torchvision import

--- src/unet_parts.py
+++ src/unet_parts.py
@@ -1,9 +1,10 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
+import torchvision
 
-class Unet_block(nn.Module):
-    """Standard block as used in the encoder and decoder in UNET paper"""
+
+class UNETBlock(nn.Module):
+    """Standard UNET block as used in the encoder and decoder in UNET paper"""
     def __init__(self, ch_in, ch_out):
         super().__init__()
         self.conv1 = nn.conv2d(ch_in, ch_out, 3)
@@ -11,38 +12,42 @@ class Unet_block(nn.Module):
         self.conv2 = nn.conv2d(ch_out, ch_out, 3)
 
     def forward(self, x):
-        """
+        """ Forward pass function for UNET_block as required when using Pytorch.
 
         Parameters
         ----------
-        x :
+        x : torch.Tensor
+            Input for the UNET block.
             
 
         Returns
         -------
-
-        
+        torch.Tensor
+            Output of the UNET block as per used in the UNET paper.
         """
         return self.relu(self.conv2(self.relu(self.conv1(x))))
 
-class UNET_encoder(nn.Module):
-    """ """
+class UNETEncoder(nn.Module):
+    """Encoder part of the UNET model as described in the UNET paper."""
     def __init__(self, channels = (3, 64, 128, 256. 512. 1024)):
         super().__init__()
-        self.encode_UNET = nn.MolduleList([Unet_block(channels[i], channels[i+1]) for i in range(len(channels)-1)])
+        self.encode_UNET = nn.ModuleList([UNETBlock(channels[i], channels[i+1]) for i in range(len(channels)-1)])
         self.mpool = nn.MaxPool2d(2)
 
     def forward(self, x):
-        """
+        """ Forward pass function for the encoder part of UNET as required when using Pytorch.
 
         Parameters
         ----------
-        x :
-            
+        x : torch.Tensor
+            Input for the UNET encoder where each input channel in the case of MICS represents an image containing
+            spatial intensity for a given marker.
 
         Returns
         -------
-
+        List[torch.Tensor, ...]
+            List of torch.Tensors where each torch.Tensor element of the list corresponds to the features of a given
+            UNET_block.
         """
         features = []
         for u_block in self.encode_UNET:
@@ -56,7 +61,7 @@ class UNET_decoder(nn.Module):
     def __init__(self, channels = (1024, 512, 256, 128, 64)):
         super().__init__()
         self.upconvs = nn.ModuleList([nn.ConvTranspose2d(channels[i], channels[i+1], 2, 2) for i in range(len(channels)-1)])
-        self.decode_UNET = nn.ModuleList([Unet_block(channels[i], channels[i+1]) for i in range(len(channels)-1)])
+        self.decode_UNET = nn.ModuleList([UNETBlock(channels[i], channels[i+1]) for i in range(len(channels)-1)])
         self.channels = channels
 
     def crop(self, encoder_feature, x):
@@ -74,8 +79,8 @@ class UNET_decoder(nn.Module):
 
         """
         _, _, H, W = x.shape
-        encoder_feature = torchvision.transforms.CenterCrop([H, W])(encoder_features)
-        return encoder_features
+        encoder_feature = torchvision.transforms.CenterCrop([H, W])(encoder_feature)
+        return encoder_feature
 
     def forward(self, x, encoder_features):
         """
