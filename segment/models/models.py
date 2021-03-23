475e47db70718fa772e58225a1c90a83ac181abc
wmv_hpomen <w-mv@hotmail.com>
2021-03-23 20:15:32 +0100

    fixed typo decoder to decode

--- segment/models/models.py
+++ segment/models/models.py
@@ -1,4 +1,4 @@
-from .unet_parts import *
+from unet_parts import *
 import torch.nn as nn
 import torch.nn.functional as F
 
@@ -11,7 +11,7 @@ class UNET(nn.Module):
         self.keep_dim = keep_dim
         self.output_size = out_size
         self.encode = UNETEncoder(encode_chs)
-        self.decoder = UNETDecoder(decode_chs)
+        self.decode = UNETDecoder(decode_chs)
         self.output = nn.Conv2d(decode_chs[-1], num_class, (1, 1))
 
     def forward(self, x):
@@ -26,8 +26,8 @@ class UNET(nn.Module):
         -------
 
         """
-        encoder_features = self.encoder(x)
-        out = self.decoder(encoder_features[::-1][0], encoder_features[::-1][1:])
+        encoder_features = self.encode(x)
+        out = self.decode(encoder_features[::-1][0], encoder_features[::-1][1:])
         out = self.output(out)
         if self.keep_dim:
             out = F.interpolate(out, self.out_size)
